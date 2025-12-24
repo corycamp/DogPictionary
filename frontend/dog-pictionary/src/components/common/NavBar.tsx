22c20388b4e24015d35b66d247efaf13737302fd
Cory <campbellcory2360@gmail.com>
2025-12-24 09:17:14 -0500

    feat:fixed tailwindcss and setup google-icons CDN

--- frontend/dog-pictionary/src/components/common/NavBar.tsx
+++ frontend/dog-pictionary/src/components/common/NavBar.tsx
@@ -1,8 +1,16 @@
 const NavBar = () => {
   return (
-    <nav className="bg-blue-600 p-4">
-      <h1 className="text-white text-2xl font-bold">Dog Pictionary</h1>
-    </nav>
+    <header className="header">
+    <div className="logo">
+          🐾 <span>Dog Pictionary</span>
+        </div>
+        <nav className="nav">
+          <a href="#">How to Play</a>
+          <a href="#">Leaderboard</a>
+          <a href="#">Sign In</a>
+          <button className="btn primary">Sign Up</button>
+        </nav>
+      </header>
   );
 };
 
